1fbd09673dd4027b8ff8a9bb70cad89a23c2683f
Francesco Leardini <piolag@gmail.com>
2020-09-08 11:33:35 +0200

    Added can I use links.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ const routes: Routes = [
   { path: 'visibility', loadChildren: () => import('./visibility/visibility.module').then(m => m.VisibilityModule) },
   { path: 'share', loadChildren: () => import('./share/share.module').then(m => m.ShareModule) },
   { path: 'vibrate', loadChildren: () => import('./vibration/vibration.module').then(m => m.VibrationModule) },
-  { path: 'home', component: HomeComponent },
   { path: 'light', loadChildren: () => import('./light/light.module').then(m => m.LightModule) },
+  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: '/home'}
 ];
 
