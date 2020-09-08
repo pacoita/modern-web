c11320255174aa06b8d5cd9f9e377c83d19b2e2e
Francesco Leardini <piolag@gmail.com>
2020-09-08 11:02:13 +0200

    Refactored ambient light api demo. Improved layout width.

--- src/app/light/light.component.ts
+++ src/app/light/light.component.ts
@@ -1,37 +1,51 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-light',
   templateUrl: './light.component.html',
-  styleUrls: ['./light.component.scss']
+  styleUrls: ['./light.component.scss'],
 })
 export class LightComponent implements OnInit {
-
   ambient: 'dark' | 'bright' = 'bright';
   unsupportedText: string | undefined;
-  lightValue = 0;
+  luxValue$ = new ReplaySubject<number>(1);
 
   @HostListener('window:devicelight', ['$event'])
   private async onDeviceLightChange(event: DeviceLightEvent): Promise<void> {
-    this.lightValue = event.value;
-    if (event.value < 50) {
-      console.log({event});
-      this.ambient = 'dark';
-    } else {
-      this.ambient = 'bright';
-    }
+    this.updateLight(event.value);
   }
 
   ngOnInit(): void {
-    if (!('ondevicelight' in window)) {
-      this.unsupportedText = 'Your browser does not support Ambient Device Light events';
+    if ('AmbientLightSensor' in window && 'ondevicelight' in window) {
+      try {
+        const sensor = new (window as any).AmbientLightSensor();
+        sensor.addEventListener('reading', (event: any) => {
+          this.updateLight(sensor.illuminance);
+        });
+        sensor.start();
+      } catch (err) {
+        console.error(err.name, err.message);
+      }
     } else {
-      window.addEventListener('devicelight', (event) => {
-        console.log({event});
-        if (event.value < 50) {
-        } else {
-        }
-      });
+      this.unsupportedText =
+        'Your browser doesn\'t support Ambient Device Light Sensor API';
     }
+  }
+
+  private updateLight(luxValue: number): void {
+    /*
+    10 ~ 50 lux : Dim Environment
+    100 ~ 1000 lux : Normal
+    > 10000 lux : Bright
+    */
+    if (luxValue < 50) {
+      console.log({ event });
+      this.ambient = 'dark';
+    } else {
+      console.log({ event });
+      this.ambient = 'bright';
+    }
+    this.luxValue$.next(luxValue);
   }
 }
