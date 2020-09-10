1e2495373fb45312e2722334347899d768795706
Francesco Leardini <piolag@gmail.com>
2020-09-10 11:50:55 +0200

    Implemented Contact Picker API demo.

--- src/app/contact-picker/contact-picker.component.ts
+++ src/app/contact-picker/contact-picker.component.ts
@@ -3,18 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-picker',
   templateUrl: './contact-picker.component.html',
-  styleUrls: ['./contact-picker.component.scss']
+  styleUrls: ['./contact-picker.component.scss'],
 })
 export class ContactPickerComponent implements OnInit {
-
   statusText: string | undefined;
+  supported = true;
 
-  constructor() { }
+  contacts: any[] = [];
+
+  constructor() {}
 
   ngOnInit(): void {
     if (!('contacts' in navigator && 'ContactsManager' in window)) {
-      this.statusText = 'Your browser doesn\'t support Contact Picker API';
+      this.supported = false;
+      this.statusText = 'Your browser does not support Contact Picker API';
     }
   }
 
+  async selectContacts(): Promise<void> {
+    // Available fields: 'name', 'email', 'tel', 'address', 'icon'
+    // Nb. address and icon fields available only in Chrome 84+
+
+    // Dynamically returns the available contact properties
+    const fields = await (navigator as any).contacts.getProperties();
+    const options = { multiple: true };
+    try {
+      this.contacts = await (navigator as any).contacts.select(fields, options);
+    } catch (err) {
+      this.statusText = `${err.name} - ${err.message}`;
+    }
+  }
 }
