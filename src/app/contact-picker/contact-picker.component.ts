2f237cb654524db74a1c8060d90ff8252df952ad
Francesco Leardini <piolag@gmail.com>
2020-09-15 10:55:20 +0200

    Corrected mat-card layout in contact picker demo.

--- src/app/contact-picker/contact-picker.component.ts
+++ src/app/contact-picker/contact-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, SecurityContext } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
