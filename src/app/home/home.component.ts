11af41c75b6a0ba21b94d2ee5c27156ee8b2513f
Francesco Leardini <francesco.leardini@trivadis.com>
2020-11-22 18:52:13 +0100

    Updated home image style.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,8 @@ import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['home.component.scss']
 })
 export class HomeComponent {
   constructor(private titleService: Title ) {
