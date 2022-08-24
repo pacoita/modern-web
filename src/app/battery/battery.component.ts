79f9fa3f7454f472a03312c9c741d9c8a127a6db
Francesco Leardini <francesco.leardini@mobi.ch>
2022-08-24 19:53:28 +0200

    Added module and route for battery-status-api

---
+++ src/app/battery/battery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-battery',
+  templateUrl: './battery.component.html',
+  styleUrls: ['./battery.component.scss']
+})
+export class BatteryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
