a60035184fef9a3ce88da74aeaa03c4cb5e704be
Francesco Leardini <piolag@gmail.com>
2024-01-05 20:19:00 +0100

    Added orintation demo

--- src/app/orientation/orientation.component.ts
+++ src/app/orientation/orientation.component.ts
@@ -8,23 +8,59 @@ import { Component, HostListener } from '@angular/core';
 export class OrientationComponent {
 
   isActive = false;
+  mess = 'INIT';
+  showDetails = false;
+  sup = false;
 
   x: number | undefined;
 
+  constructor() {
+    // if (window.DeviceOrientationEvent) {
+    //   this.sup = true;
+    //   window.addEventListener(
+    //     "deviceorientation",
+    //     (event) => {
+    //       const frontToBack = event.beta || 0; // beta: front back motion
+    //       this.doLogic(Math.round(frontToBack));
+    //     },
+    //     true,
+    //   );
+    // } else {
+    //   this.sup = false;
+    //   this.mess = "Sorry, your browser doesn't support Device Orientation";
+    // }
+  }
+
   @HostListener('window:deviceorientation', ['$event'])
-  private async onOrientationChange(event: any): Promise<void> {
-      let x = event.beta; // In degree in the range [-180,180)
-   
-      console.log('current value: ', x);
-      
-      if (x > 160) {
-        this.isActive = true;
-        console.log('Activate: ', x);
-      } else if (x < 100 && this.isActive) {
-        this.isActive = false;
-        console.log('toggle: ', x);
-      }
-    
+  private onOrientationChange(event: DeviceOrientationEvent) {
+    let x = event.beta || 0; // In degree in the range [-180,180)
+    this.doLogic(x);
+
+  }
+
+  doLogic(x: number) {
+    this.mess = 'inside';
+    // To prevent the cases when the device is upside down
+    // the x value is limited
+
+    if (x < -40) {
+      x = 0;
+    }
+    this.x = x;
+    console.log('current value: ', x);
+    if (!x) {
+      return;
+    }
+
+    if (x > 160) {
+      this.isActive = true;
+      this.mess = '' + x;
+      console.log('Activate: ', x);
+    } else if (x < 70 && this.isActive) {
+      this.isActive = false;
+      this.showDetails = !this.showDetails;
+      console.log('toggle: ', x);
+    }
   }
 
 }
