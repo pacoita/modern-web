48d4f7446dbee732133568702ceba6fd91beb428
Francesco Leardini <piolag@gmail.com>
2024-01-07 12:07:48 +0100

    Added todo points for future refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  // TODO: 
+  // 1- Make all components standalone
+  // 2- Add also alpha and gamma examples to the orientation component
 }
