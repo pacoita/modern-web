dfeb4c85c57e22f27f927d8f6d8d2bfece308a4c
Francesco Leardini <piolag@gmail.com>
2024-01-18 10:31:44 +0100

    Added details section and vars for demo. Updated home text

--- src/app/orientation/orientation.component.ts
+++ src/app/orientation/orientation.component.ts
@@ -1,39 +1,48 @@
 import { Component, HostListener } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-orientation',
     templateUrl: './orientation.component.html',
     styleUrls: ['./orientation.component.scss'],
     standalone: true,
-    imports: [MatCardModule, MatExpansionModule]
+    imports: [MatCardModule, MatExpansionModule, MatCheckboxModule, FormsModule]
 })
 export class OrientationComponent {
 
   isActive = false;
   showDetails = false;
+  showDegrees = false;
+  xValue: number = 0;
 
   @HostListener('window:deviceorientation', ['$event'])
   private onOrientationChange(event: DeviceOrientationEvent): void {
     // event.beta value is given in degree in the range [-180,180)
-    let xValue = event.beta;
-    if (!xValue) {
+    this.xValue = Math.round(event.beta ?? 0);
+    if (!this.xValue) {
       return;
     }
 
-    // To prevent the cases when the device is upside down the x value is partially limited
-    if (xValue < -40) {
-      xValue = 0;
+    const isExcludedRange = this.xValue < -40 && this.xValue > -130;
+    const isActiveRange = this.xValue >= -40 || this.xValue < 50;
+
+    // To exclude the cases when the device is upside down the x value is partially limited
+    if (isExcludedRange) {
+      this.xValue = 0;
     }
 
-    // Here 70 and 160 degrees values are respectively the threshold to trigger the details toggling and the threshold to reset the flag.
-    // These values allow to enable the demo logic and still be "comfortable" while using the mobile device.
-    if (xValue > 160) {
-      // The device is facing down, set isActive to true to trigger details toggling once position is restored
+    // 50 and 160 values are respectively the threshold to trigger the details toggling and to reset the flag.
+    // These values allow enabling the demo logic, while still remaining "comfortable" for the user.
+    if (this.xValue > 160) {
+      // The device is facing down 
+      // The flag isActive set to true to trigger details toggling once position is restored
       this.isActive = true;
-    } else if (xValue < 70 && this.isActive) {
-      // If the device is no more upside down and isActive is true, toggle the details and reset the flag
+    } else if (isActiveRange && this.isActive) {
+      // The device is now facing up
+      // If the flag isActive is true, toggle the details and reset the flag
       this.isActive = false;
       this.showDetails = !this.showDetails;
     }
