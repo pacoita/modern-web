3c7ac16d51f68b117d033ea84b546769ecd66fb7
Francesco Leardini <piolag@gmail.com>
2025-05-26 16:52:16 +0200

    Added reactive logic on options change

--- src/app/summarize/summarize.component.ts
+++ src/app/summarize/summarize.component.ts
@@ -1,13 +1,25 @@
 /// <reference types="dom-chromium-ai"/>
-import { Component, computed, OnInit, signal, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, OnInit, signal, effect, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 
+// Declare Summarizer, LanguageModel and ai as globals, to avoid the TS compiler complaining about
+// unknown objects in the global scope.
+declare global {
+  interface Window {
+    Summarizer: any,
+    LanguageModel: any,
+    ai: any,
+  }
+}
+
 @Component({
   selector: 'app-summarize',
   imports: [
+    CommonModule,
     MatCardModule,
     MatButtonModule,
     MatExpansionModule,
@@ -17,6 +29,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
   styleUrl: './summarize.component.scss'
 })
 export class SummarizeComponent implements OnInit {
+  @ViewChild('textbox') textbox?: ElementRef<HTMLTextAreaElement>;
   unsupportedText?: string;
   summarizedText?: string;
   isSummarizing = false;
@@ -24,7 +37,7 @@ export class SummarizeComponent implements OnInit {
 
   outputTypes = [
     { value: 'key-points', label: 'Key Points' },
-    { value: 'tl;dr', label: 'TL;DR' },    
+    { value: 'tldr', label: 'TL;DR' },
     { value: 'teaser', label: 'Teaser' },
     { value: 'headline', label: 'Headline' },
   ];
@@ -37,59 +50,64 @@ export class SummarizeComponent implements OnInit {
     { value: 'medium', label: 'Medium' },
     { value: 'long', label: 'Long' },
   ];
+  // typesDescriptions = [
+  //   { value: 'key-points', description: 'Summary should extract the most important points from the input, presented as a bulleted list.' },
+  //   { value: 'tldr', description: 'Summary should be short and to the point, providing a quick overview of the input, suitable for a busy reader.' },
+  //   { value: 'teaser', description: 'Summary should focus on the most interesting or intriguing parts of the input, designed to draw the reader in to read more.' },
+  //   { value: 'headline', description: 'Summary should effectively contain the main point of the input in a single sentence, in the format of an article headline.' },
+  // ];
   selectedFormat = signal('plain-text');
-  selectedLength = signal('long'); 
-  selectedType = signal('tl;dr');
+  selectedLength = signal('long');
+  selectedType = signal('tldr');
 
   summarizeOptions = computed(() => {
     return {
-      type: this.selectedType(),
       format: this.selectedFormat(),
+      type: this.selectedType(),
       length: this.selectedLength(),
     };
   });
 
+  constructor() {
+    effect(async () => {      
+      // Effect should be called whenever the summarizeOptions changes but skipping the initial execution.
+      let newOptions = this.summarizeOptions()
+      if(this.summarizer) {
+        this.summarizer = await (self.Summarizer).create(newOptions);
+        this.summarizeText(this.textbox?.nativeElement?.value || '');
+      }
+    });
+  }
+
   async ngOnInit() {
     if ('Summarizer' in self) {
-      // TODO: : Once set, the parameters can't be changed. Create a new summarizer object if you need to make modifications to the parameters.
-      // const options: AISummarizerCreateOptions = {
-      //   // sharedContext: 'This is a {targetContext} article', // Optional context
-      //   type: 'tl;dr', // Type of summarization: 'key-points', 'tl;dr', 'teaser', or 'headline'
-      //   format: 'plain-text', // Format: 'markdown' or 'plain-text'
-      //   length: 'long' 
-      // };      
-
       /**
-       * AISummarizerCapabilities object with an available field that can take three possible values
-       * 
        * - no: The current browser supports the Summarizer API, but it can't be used at the moment. This could be for a number of reasons, such as insufficient available disk space to download the model.
          - readily: The current browser supports the Summarizer API, and it can be used right away.
          - after-download: The current browser supports the Summarizer API, but it needs to download the model first.
        */
-      const available = (await ((self as any).Summarizer).availability());
+      const available = await (self.Summarizer).availability();
       if (available === 'no') {
         this.unsupportedText = "The Summarizer API isn't usable. This could be for a number of reasons, such as insufficient available disk space to download the model.";
         return;
       }
       if (available === 'readily') {
-        this.summarizer = await ((self as any).Summarizer).create(this.summarizeOptions());
+        if (!this.summarizer) {
+          this.summarizer = await (self.Summarizer).create(this.summarizeOptions());
+        }
       } else {
-        // The Summarizer API can be used after the model is downloaded.
-        // TODO: Add a prompt before downloading the model.
-        this.summarizer = await ((self as any).Summarizer).create({
-          monitor(m: AICreateMonitor) {
-            m.addEventListener('downloadprogress', (e: DownloadProgressEvent) => {
-              console.log(`Downloaded ${e.loaded} of ${e.total} bytes.`);
-            });
-          }
-        });
-        await (this.summarizer as any)?.ready;
-      }
-
-
-    effect(async () => {
-      this.summarizer = await ((self as any).Summarizer).create(this.summarizeOptions());
-    });
+        // The Summarizer API can be used after the model is downloaded
+        if (!this.summarizer) {
+          this.summarizer = await (self.Summarizer).create({
+            monitor(m: AICreateMonitor) {
+              m.addEventListener('downloadprogress', (e: DownloadProgressEvent) => {
+                console.log(`Downloaded ${e.loaded} of ${e.total} bytes.`);
+              });
+            }
+          });
+          await (this.summarizer as any)?.ready;
+        }
+      }     
     } else {
       this.unsupportedText = 'The Summarizer API is not supported in your browser.';
     }
